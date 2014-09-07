e8faef6ca9c5468dc69d23906df383d85587a5a6
chenzhipeng901105@gmail.com <chenzhipeng901105@gmail.com>
2014-09-08 00:15:20 +0800

    add download session

--- NASA TV/VideoDetailViewController.m
+++ NASA TV/VideoDetailViewController.m
@@ -14,6 +14,11 @@
 
 @property (strong, nonatomic) NSURL* videoURL;
 @property (strong, nonatomic) MPMoviePlayerController* moviePlayerViewController;
+
+@property (strong, nonatomic) NSURLSession *urlSession;
+@property (strong, nonatomic) NSURLSessionDownloadTask *downloadTask;
+@property (strong, nonatomic) NSString *videosDerectoryPath;
+
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *downloadButton;
 @property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 - (IBAction)downloadButtonTapped:(id)sender;
@@ -41,6 +46,9 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
+    self.progressView.progress = 0.0f;
+    self.progressView.hidden = YES;
     
     self.moviePlayerViewController = [[MPMoviePlayerController alloc] initWithContentURL:self.videoURL];
     [self.moviePlayerViewController prepareToPlay];
@@ -74,6 +82,64 @@
     }
 }
 
+- (NSString *)videosDerectoryPath
+{
+    if (!_videosDerectoryPath) {
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+        _videosDerectoryPath = [paths[0] stringByAppendingPathComponent:@"com.razeware.videos"];
+        BOOL directoryExists = [[NSFileManager defaultManager] fileExistsAtPath:_videosDerectoryPath];
+        
+        if (!directoryExists) {
+            NSError *error;
+            if (![[NSFileManager defaultManager] createDirectoryAtURL:_videosDerectoryPath withIntermediateDirectories:NO attributes:nil error:&error]) {
+                
+            }
+        }
+    }
+    return _videosDerectoryPath;
+}
+
+#pragma mark - NSURLSessionDownloadTask methods
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.progressView.progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
+    });
+}
+
 - (IBAction)downloadButtonTapped:(id)sender {
+    
+    // 1
+    /*
+     *  this property indecate a video has already been downloaded and prevents you from downloading it twice
+     */
+    if ([self.video.availableOffline boolValue]) {
+        return;
+    }
+    
+    // 2
+    /*
+     *  disable the download button while the download operation is in progress to prevent launching a duplicate NSURLSessionDownloadTask;
+     */
+    self.downloadButton.enabled = NO;
+    self.progressView.hidden = NO;
+    
+    // 3
+    /*
+     *  set up NSURLSession config
+     */
+    if (!self.urlSession) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        self.urlSession = [NSURLSession sessionWithConfiguration:config
+                                                        delegate:self
+                                                   delegateQueue:[NSOperationQueue mainQueue]];
+    }
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.videoURL];
+    
+    self.downloadTask = [self.urlSession downloadTaskWithRequest:request];
+    
+    // 4
+    [self.downloadTask resume];
 }
 @end
